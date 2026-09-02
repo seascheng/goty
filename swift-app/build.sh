6d5d3a7148cec6ad1d9a72c09025f04f47f3bb67
Seas <chengweipeng123@163.com>
2026-09-02 17:17:24 +0800

    release: bump version heredoc to 0.2.0 (build 3)

--- swift-app/build.sh
+++ swift-app/build.sh
@@ -126,8 +126,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundleName</key><string>Goty AI</string>
     <key>CFBundleDisplayName</key><string>Goty AI</string>
     <key>CFBundleIdentifier</key><string>com.goty.ai</string>
-    <key>CFBundleVersion</key><string>2</string>
-    <key>CFBundleShortVersionString</key><string>0.1.1</string>
+    <key>CFBundleVersion</key><string>3</string>
+    <key>CFBundleShortVersionString</key><string>0.2.0</string>
     <key>CFBundleExecutable</key><string>goty</string>
     <key>CFBundlePackageType</key><string>APPL</string>
     <key>LSMinimumSystemVersion</key><string>13.0</string>
