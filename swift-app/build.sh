d777d5b846e35a50137fa38d7f7cd34b6a39148f
Seas <chengweipeng123@163.com>
2026-09-15 12:58:57 +0800

    release: bump 0.3.1 (build 5)

--- swift-app/build.sh
+++ swift-app/build.sh
@@ -128,8 +128,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundleName</key><string>Goty AI</string>
     <key>CFBundleDisplayName</key><string>Goty AI</string>
     <key>CFBundleIdentifier</key><string>com.goty.ai</string>
-    <key>CFBundleVersion</key><string>4</string>
-    <key>CFBundleShortVersionString</key><string>0.3.0</string>
+    <key>CFBundleVersion</key><string>5</string>
+    <key>CFBundleShortVersionString</key><string>0.3.1</string>
     <key>CFBundleExecutable</key><string>goty</string>
     <key>CFBundlePackageType</key><string>APPL</string>
     <key>LSMinimumSystemVersion</key><string>13.0</string>
